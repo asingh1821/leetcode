c6ed81e17c363b06ccc0f8aba1fb8a1c8d021f26
asingh1821 <kkamit777@gmail.com>
2024-11-05 21:47:54 +0530

    first commit

---
+++ src/Example/ds/FindFirstDifferentCharacter.java
@@ -0,0 +1,31 @@
+package Example.ds;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindFirstDifferentCharacter {
+    public static void main(String[] args) {
+        String s = "amitsamingh";
+         char result = findFirstDifferent(s);
+        System.out.println(result);
+    }
+    public static Character findFirstDifferent(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(map.containsKey(c)){
+                map.put(c, map.get(c)+1);
+            }else {
+                map.put(c, 1);
+            }
+        }
+        System.out.println(map);
+        for (int i = 0; i < s.length(); i++) {
+            char n = s.charAt(i);
+            if (map.get(n) == 1){
+                return n;
+            }
+        }
+        return  0;
+    }
+}
